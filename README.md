268b5a048db2d63130506cbe20b5b84718e3abea
pengdefeng <2450410477@qq.com>
2025-07-12 19:51:38 +0800

    base

---
+++ README.md
@@ -0,0 +1,4 @@
+# 基于ViT的新冠肺炎肺部分类系统
+## ViT理论篇
+参考学习链接：https://blog.csdn.net/qq_37541097/article/details/117691873
+## 项目介绍
---
+++ model/__init__.py
@@ -0,0 +1,10 @@
+import sys
+sys.path.append(r"E:\transformer")
+from model.vit import vit_base_patch16_224
+
+model_dict = {
+    'vit':vit_base_patch16_224,
+}
+
+def create_model(model_name, num_classes):   
+    return model_dict[model_name](num_classes = num_classes)
